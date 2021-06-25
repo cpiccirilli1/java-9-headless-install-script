413bf2cbdfb167700b221cfe239e027657b49099
cpiccirilli1 <Piccirilli115@gmail.com>
2021-06-25 16:44:04 -0400

    Final Commit

---
+++ java9headlessinstall.sh
@@ -0,0 +1,5 @@
+pkg install wget -y
+wget https://archive.org/download/openjdk-9-jre-headless_9.2017.8.20-1_x86_64/openjdk-9-jdk-headless_9.2017.8.20-1_arm.deb
+wget https://archive.org/download/openjdk-9-jre-headless_9.2017.8.20-1_x86_64/openjdk-9-jre-headless_9.2017.8.20-1_arm.deb
+apt-get install -y ./openjdk-9-jre-headless_9.2017.8.20-1_arm.deb
+apt-get install -y ./openjdk-9-jdk-headless_9.2017.8.20-1_arm.deb
